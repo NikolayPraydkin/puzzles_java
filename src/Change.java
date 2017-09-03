7c366a438ff8cd1103425055ec71d12d294ebce2
Nikolai <tamerlan22vek@gmail.com>
2017-09-03 17:40:42 +0300

    first commit

---
+++ src/Change.java
@@ -0,0 +1,7 @@
+public class Change {
+    public static void main(String[] args) {
+        //wrong
+        System.out.println(2.00 - 1.10);
+    }
+
+}
---
+++ src/Oddarguments.java
@@ -0,0 +1,14 @@
+public class Oddarguments {
+
+
+    public static void main(String[] args) {
+        System.out.println(isOdd(-1024));
+
+    }
+
+    public static boolean isOdd(int key) {
+
+        return (key & 1) == 0;
+
+    }
+}
